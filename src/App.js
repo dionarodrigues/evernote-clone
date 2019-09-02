8e10baabc7f53be6ae8baf089aebe274dfcdbcbb
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2019-09-01 22:43:40 -0300

    Creating the project adding firebase, helpers and some initial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebase = require('firebase');
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      selectedNodeIndex: null,
+      selectedNote: null,
+      notes: null
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        Hello World
+      </div>
+    )
+  }
+
+  componentDidMount = () => {
+    firebase 
+      .firestore()
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data()
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes)
+        this.setState({ notes: notes })
+      });
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDdY5NZk-HT-e4nGa9P_L64bLsywBsMqX4",
+  authDomain: "evernote-clone-749a8.firebaseapp.com",
+  databaseURL: "https://evernote-clone-749a8.firebaseio.com",
+  projectId: "evernote-clone-749a8",
+  storageBucket: "evernote-clone-749a8.appspot.com",
+  messagingSenderId: "67497143951",
+  appId: "1:67497143951:web:628d8d5a5bf587bc"
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
