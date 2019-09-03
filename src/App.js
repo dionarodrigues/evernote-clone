1e72983455d228a9ca40b3027b16a5a8d2b03fda
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2019-09-03 16:05:45 -0300

    Adding new note proccess

--- src/App.js
+++ src/App.js
@@ -19,8 +19,38 @@ class App extends Component {
     this.deleteNote = this.deleteNote.bind(this);
   }
 
-  newNote() {
-    console.log(this.state)
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data()
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes)
+        this.setState({ notes: notes })
+      });
+  }
+
+  async newNote(title) {
+    const note = {
+      title: title,
+      body: ''
+    };
+    const newFromDB = await firebase
+      .firestore()
+      .collection('notes')
+      .add({
+        title: note.title,
+        body: note.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+    const newID = newFromDB.id;
+    await this.setState({ notes: [...this.state.notes, note] });
+    const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]);
+    this.setState({ selectedNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex });
   }
 
   selectNote = (note, index) => this.setState({ selectedNoteIndex: index, selectedNote: note });
@@ -43,11 +73,11 @@ class App extends Component {
 
   render() {
 
-    const { 
-      notes, 
+    const {
+      notes,
       selectedNoteIndex,
       selectedNote,
-      newNote 
+      newNote
     } = this.state;
 
     return (
@@ -57,36 +87,21 @@ class App extends Component {
           selectedNoteIndex={selectedNoteIndex}
           selectNote={this.selectNote}
           deleteNote={this.deleteNote}
-          newNote={newNote}
+          newNote={this.newNote}
         />
 
         {
           selectedNote &&
-          <EditorComponent 
+          <EditorComponent
             selectedNote={selectedNote}
             noteUpdate={this.noteUpdate}
             selectedNoteIndex={selectedNoteIndex}
             notes={notes}
           />
         }
-        
+
       </div>
     )
-  }
-
-  componentDidMount = () => {
-    firebase
-      .firestore()
-      .collection('notes')
-      .onSnapshot(serverUpdate => {
-        const notes = serverUpdate.docs.map(_doc => {
-          const data = _doc.data()
-          data['id'] = _doc.id;
-          return data;
-        });
-        console.log(notes)
-        this.setState({ notes: notes })
-      });
   }
 
 }
--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -12,14 +12,14 @@ class SidebarComponent extends Component {
       addingNote: false,
       title: null
     };
-    this.newNoteBtnClick = this.newNoteBtnClick.bind(this);
+    this.addNoteBtn = this.addNoteBtn.bind(this);
     this.uptadeTitle = this.uptadeTitle.bind(this);
-    this.newNote = this.newNote.bind(this);
+    this.submitNewNote = this.submitNewNote.bind(this);
     this.handleSelectNote = this.handleSelectNote.bind(this);
     this.handleDeleteNote = this.handleDeleteNote.bind(this);
   }
 
-  newNoteBtnClick() {
+  addNoteBtn() {
     this.setState({
       title: null,
       addingNote: !this.state.addingNote
@@ -32,11 +32,13 @@ class SidebarComponent extends Component {
     })
   }
 
-  newNote() {
-    console.log(this.state)
+  submitNewNote() {
+    this.props.newNote( this.state.title );
+    this.setState({ addingNote: false, title: null })
   }
 
   handleSelectNote = (note, index) => this.props.selectNote(note, index);
+  
   handleDeleteNote = (note) => this.props.deleteNote(note);
 
   render() {
@@ -46,7 +48,7 @@ class SidebarComponent extends Component {
     return(
       <div className={classes.sidebarContainer}>
         <Button
-          onClick={this.newNoteBtnClick}
+          onClick={this.addNoteBtn}
           className={classes.newNoteBtn}>
             { this.state.addingNote ? 'Cancel' : 'Add Note' }
         </Button>
@@ -59,7 +61,7 @@ class SidebarComponent extends Component {
               placeholder='Enter note title'
               onKeyUp={(e) => this.uptadeTitle(e.target.value)} />
             <Button
-              onClick={this.newNote}
+              onClick={this.submitNewNote}
               className={classes.newNoteSubmitBtn}>
                 Submit Note
             </Button>
